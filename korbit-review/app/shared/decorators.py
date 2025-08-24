12f19ac05ea4f9762cfff6105547be598295def5
Bilal K. <lalbikhan2014@gmail.com>
2025-08-25 03:53:11 +0500

    commit

---
+++ korbit-review/app/shared/decorators.py
@@ -0,0 +1,37 @@
+import functools
+
+from fastapi import HTTPException
+
+
+# Return if error happens
+def return_on_error(return_type):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except (AttributeError, IndexError):
+                return return_type
+
+        return wrapper
+
+    return decorator
+
+
+# Return if page not found
+def return_on_404():
+    def decorator(func):
+        @functools.wraps(func)
+        async def wrapper(*args, **kwargs):
+            try:
+                return await func(*args, **kwargs)
+            # propagates HTTPException from function
+            except HTTPException as http_exception:
+                raise http_exception
+            # catches all other execptions
+            except Exception:
+                raise HTTPException(status_code=404, detail="Page not found")
+
+        return wrapper
+
+    return decorator
